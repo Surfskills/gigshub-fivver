dec6a6fb3a37a3fc3799a6ab6254aa347354f30d
Surfskills <ojjfred@gmail.com>
2026-02-20 12:24:40 +0300

    Updated next.config

--- next.config.js
+++ next.config.js
@@ -6,36 +6,6 @@ const nextConfig = {
     },
     optimizePackageImports: ['recharts'],
   },
-
-  async redirects() {
-    return [
-      // Redirect www → non-www
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.mini-gigs-hub.space',
-          },
-        ],
-        destination: 'https://mini-gigs-hub.space/:path*',
-        permanent: true,
-      },
-      // Redirect HTTP → HTTPS
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: 'http',
-          },
-        ],
-        destination: 'https://mini-gigs-hub.space/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
